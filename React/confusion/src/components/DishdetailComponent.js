2b0c01531fc031722857b644a61fee1ef0b8aab4
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2020-10-04 18:57:47 +0300

    create simple json-server and use fetch

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -10,6 +10,7 @@ import {
 
 
 import { Loading } from "./LoadingComponent";
+import {baseUrl} from "../shared/baseUrl";
 
 const maxLength = (len) => (val) => !val || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -17,7 +18,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
 function RenderDish({dish}) {
   return (
       <Card>
-        <CardImg top src={dish.image} alt={dish.name}/>
+        <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
@@ -26,7 +27,7 @@ function RenderDish({dish}) {
   )
 }
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
   const commentsArr = comments.map((comment) => {
     return (
         <li>
@@ -42,7 +43,7 @@ function RenderComments({comments, addComment, dishId}) {
         <ul className='list-unstyled'>
           {commentsArr}
         </ul>
-        <CommentForm dishId={dishId} addComment={addComment}/>
+        <CommentForm dishId={dishId} postComment={postComment}/>
       </>
   )
 }
@@ -83,7 +84,7 @@ const DishDetail = (props) => {
               <RenderDish dish={props.dish} />
             </div>
             <div className="col-12 col-md-5 m-1">
-              <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id}/>
+              <RenderComments comments={props.comments} postComment={props.postComment} dishId={props.dish.id}/>
             </div>
           </div>
         </div>
@@ -113,7 +114,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
   }
 
   render() {
@@ -148,7 +149,7 @@ class CommentForm extends Component {
                                     maxLength: maxLength(15)
                                   }}
                     />
-                    <Errors className="text-danger" model=".your-name" show="touched"
+                    <Errors className="text-danger" model=".author" show="touched"
                             messages={{
                               minLength: 'Must be greater than 2 characters',
                               maxLength: 'Must be 15 characters or less'
--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -17,7 +17,7 @@ import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 
-import { addComment, fetchDishes } from "../redux/ActionCreators";
+import {postComment, fetchComments, fetchDishes, fetchPromos} from "../redux/ActionCreators";
 
 const mapStateToProps = state => {
   return {
@@ -29,11 +29,11 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
-  addComment: (dishId, rating, author, comment) => {
-    dispatch(addComment(dishId, rating, author, comment))
-  },
   fetchDishes: () => {dispatch(fetchDishes())},
-  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))},
+  fetchComments: () => {dispatch(fetchComments())},
+  fetchPromos: () => {dispatch(fetchPromos())},
+  postComment: (dishId, rating, author, comment) => {dispatch(postComment(dishId, rating, author, comment))}
 });
 
 
@@ -45,6 +45,8 @@ class Main extends Component {
 
   componentDidMount() {
     this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
   }
 
 
@@ -56,7 +58,9 @@ class Main extends Component {
             dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
             dishesLoading={this.props.dishes.isLoading}
             dishesErrMess={this.props.dishes.errMess}
-            promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+            promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+            promoLoading={this.props.promotions.isLoading}
+            promoErrMess={this.props.promotions.errMess}
             leader={this.props.leaders.filter((leader) => leader.featured)[0]}
           />
       );
@@ -68,8 +72,9 @@ class Main extends Component {
           <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
                       isLoading={this.props.dishes.isLoading}
                       errMess={this.props.dishes.errMess}
-                      comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-                      addComment={this.props.addComment} />
+                      comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                      commentsErrMess={this.props.comments.errMess}
+                      postComment={this.props.postComment} />
       );
     };
 
