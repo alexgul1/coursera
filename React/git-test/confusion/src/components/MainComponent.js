9246ac4ed37f83e6a875f725263c7c51ae5b427d
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2020-10-04 17:11:02 +0300

    create redux form and validation

--- React/git-test/confusion/src/components/MainComponent.js
+++ React/git-test/confusion/src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import {Redirect, Route, Switch, withRouter} from "react-router-dom";
 import {connect} from 'react-redux';
+import { actions } from "react-redux-form";
 
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -31,7 +32,8 @@ const mapDispatchToProps = dispatch => ({
   addComment: (dishId, rating, author, comment) => {
     dispatch(addComment(dishId, rating, author, comment))
   },
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
 });
 
 
@@ -78,7 +80,7 @@ class Main extends Component {
             <Route path="/home" component={HomePage} />
             <Route exact path="/menu" component={()=> <Menu dishes = {this.props.dishes} />} />
             <Route path="/menu/:dishId" component={DishWithId} />
-            <Route exact path="/contactus" component={Contact} />
+            <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
             <Route exact path="/aboutus" component={() => <About leaders = {this.props.leaders} />} />
             <Redirect to="/home" />
           </Switch>
