82915aee32c143b7002b58aa22c2a0d9f63d84c9
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2020-10-17 16:17:12 +0300

    added facebook auth

--- ServerSideDev/conFusionServer/config.js
+++ ServerSideDev/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey' : '12345-67890-09876-54321',
-  'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+  'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientId: '407712180221976',
+    clientSecret: '724410143af70486ebd47b61186a4a7b'
+  }
 }
