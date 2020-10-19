6d223471ad226d3c515c11085989d70a295cf8ee
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2020-10-19 21:16:13 +0300

    added favorite router

--- ServerSideDev/conFusionServer/app.js
+++ ServerSideDev/conFusionServer/app.js
@@ -18,6 +18,7 @@ const dishRouter = require("./routes/dishRouter");
 const leaderRouter = require("./routes/leaderRouter");
 const promoRouter = require("./routes/promoRouter");
 const uploadRouter = require("./routes/uploadRouter");
+const favoriteRouter = require("./routes/favoriteRouter");
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
@@ -59,6 +60,7 @@ app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ ServerSideDev/conFusionServer/routes/favoriteRouter.js
@@ -0,0 +1,144 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const authenticate = require('../authenticate');
+const Favorites = require('../models/favorite');
+
+const favoriteRouter = express.Router();
+
+favoriteRouter.use(bodyParser.json());
+
+favoriteRouter.route("/")
+    .get(authenticate.verifyUser, (req, res, next) => {
+      Favorites.findOne({user: req.user._id})
+          .populate('user')
+          .populate('dishes')
+          .then((favorites) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(favorites);
+          }, (err) => {
+            next(err);
+          })
+          .catch((err) => {
+            next(err);
+          })
+    })
+    .post(authenticate.verifyUser, (req, res, next) => {
+      Favorites.findOne({user: req.user._id})
+          .then((favorites) => {
+            if (!favorites) {
+              Favorites.create({user: req.user._id, dishes: []})
+                  .then((favorites) => {
+                    req.body.forEach((favorite) => {
+                      if (favorites.dishes.indexOf(favorite._id) === -1) {
+                        favorites.dishes.push(favorite);
+                      }
+                    })
+                    favorites.save()
+                        .then((favorites) => {
+
+                          res.statusCode = 200;
+                          res.setHeader('Content-Type', 'application/json');
+                          res.json(favorites);
+                        })
+                  })
+            } else {
+              req.body.forEach((favorite) => {
+                if (favorites.dishes.indexOf(favorite._id) === -1) {
+                  favorites.dishes.push(favorite);
+                }
+              })
+              favorites.save()
+                  .then((favorites) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(favorites);
+                  })
+            }
+          }, (err) => {
+            next(err)
+          })
+          .catch((err) => {
+            next(err);
+          })
+    })
+    .delete(authenticate.verifyUser, (req, res, next) => {
+      Favorites.findOneAndDelete({user: req.user._id})
+          .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(resp);
+          }, (err) => {
+            next(err);
+          })
+          .catch((err) => {
+            next(err);
+          });
+    })
+
+favoriteRouter.route("/:dishId")
+    .post(authenticate.verifyUser, (req, res, next) => {
+      Favorites.findOne({user: req.user._id})
+          .then((favorites) => {
+            if (!favorites) {
+              Favorites.create({user: req.user._id, dishes: []})
+                  .then((favorites) => {
+                    favorites.dishes.push(req.params.dishId);
+                    favorites.save()
+                        .then((favorites) => {
+                          res.statusCode = 200;
+                          res.setHeader('Content-Type', 'application/json');
+                          res.json(favorites);
+                        })
+                  })
+            } else {
+              if (favorites.dishes.indexOf(req.params.dishId) === -1) {
+                favorites.dishes.push(req.params.dishId);
+                favorites.save()
+                    .then((favorites) => {
+                      res.statusCode = 200;
+                      res.setHeader('Content-Type', 'application/json');
+                      res.json(favorites);
+                    })
+              } else {
+                res.statusCode = 200;
+                res.end("Favorite already added!")
+              }
+            }
+
+          }, (err) => {
+            next(err)
+          })
+          .catch((err) => {
+            next(err);
+          })
+    })
+    .delete(authenticate.verifyUser, (req, res, next) => {
+      Favorites.findOne({user: req.user._id})
+          .then((favorites) => {
+            const index = favorites.dishes.indexOf(req.params.dishId)
+            if (index !== -1) {
+              favorites.dishes.splice(index, 1);
+              favorites.save()
+                  .then(() => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(favorites);
+                  })
+            } else {
+              err = new Error(`Favorite ${req.params.dishId} not found`);
+              err.status = 404;
+              return next(err);
+            }
+
+          }, (err) => {
+            next(err)
+          })
+          .catch((err) => {
+            next(err);
+          })
+    })
+
+module.exports = favoriteRouter;
